5567b2f4829c9fcf95f00e894306906167831a83
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-06-09 02:32:03 -0400

    Refactor "Todo List app" to use OOP principles by replacing arrays with task objects to encapsulate task state and behaviour updating "newTask" function to create task objects with methods for logging state and marking completion and simplifying driver code to use task object methods

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
+// ARRAYS TO KEEP TRACK OF EACH TASK'S STATE
+/*const taskTitles = [];
 const taskComplete = [];
+const taskDescriptions = [];
+// CODE STARTING TO GET MESSY, THERE ARE TOO MANY ARRAYS!!
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
+  taskDescriptions.push(description);
   taskComplete.push(false);
 }
 
@@ -28,4 +31,68 @@ newTask("Do Laundry"); // task 1
 
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+logTaskState(0); // Clean Cat Litter has been completed*/
+
+// USE AN OBJECT TO KEEP TRACK OF THE STATE INSTEAD!
+/*function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+  return task;
+}
+
+
+// prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided taask as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+console.log(tasks);*/
+
+// TURN THE 2 TASK FUNCTIONS INTO TASK OBJECT METHODS TO MAKE THE CODE OBJECT ORIENTED!
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    // prints out the provided task's details
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    // marks the provided taask as completed
+    markCompleted: function() {
+      task.complete = true;
+    },
+  };
+  return task;
+}
+
+// DRIVER CODE CHANGES BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
